b8fcae0d1e3f372bad26344fd82b294d1a64350a
SaravananLal <l.saravanan92@gmail.com>
2023-01-19 08:22:40 +0530

    #Q3 Update Question

--- Q3/src/com/q3/abstrac/Bird.java
+++ Q3/src/com/q3/abstrac/Bird.java
@@ -2,23 +2,23 @@ package com.q3.abstrac;
 
 public class Bird extends Animal {
 
-	int id;
-	String name;
- Bird()
- {
-	 this();
- }
- Bird(int id, String name)
- {
-	 this.id=id;
-	 this.name=name;
-	 
- }
+	String state;
+	String habit;
+	String eating;
+
  
+	public Bird(String state, String habit, String eating) {
+		super();
+		this.state = state;
+		this.habit = habit;
+		this.eating = eating;
+	}
+
+
 	@Override
 	public void eat() {
 		// TODO Auto-generated method stub
-		System.out.println("Bird is eating-----------");
+		System.out.println("Bird is eating-----------"+this.eating);
 		
 	}
 
@@ -26,12 +26,12 @@ public class Bird extends Animal {
 	public void sleep() {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Bird is sleeping----------");
+		System.out.println("Bird is sleeping--------"+this.state);
 		
 	}
 	public void fly()
 	{
-		System.out.println("Bird ---------come under child class Animal");
+		System.out.println("Bird is flying ---------"+this.habit);
 	}
 
 }
--- Q3/src/com/q3/abstrac/ExecutionmainClass.java
+++ Q3/src/com/q3/abstrac/ExecutionmainClass.java
@@ -1,5 +1,12 @@
 package com.q3.abstrac;
 
 public class ExecutionmainClass {
+	public static void main(String args[])
+	{
+		Bird bb= new Bird("Sleep","Flying","food");
+		bb.eat();
+		bb.fly();
+		bb.sleep();;
+	}
 
 }
